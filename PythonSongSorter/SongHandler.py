ffcd5fcf83f67da6fd7af4b7184db5bd5de06e5e
nmeskell <nathanmeskell@gmail.com>
2024-06-05 21:23:07 -0500

    html update

--- PythonSongSorter/SongHandler.py
+++ PythonSongSorter/SongHandler.py
@@ -2,6 +2,7 @@ import re
 import sqlite3
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from datetime import datetime
 
 client_id = "287b3a7af7ba4a9ebb150c3c3a2ac422"
 client_secret = "ae332416541d4257826c5304953cb959"
@@ -24,7 +25,11 @@ conn = sqlite3.connect("songs.db", check_same_thread=False)
     database - (usually user inputs)
 '''
 
-def addArtist(artistID, spotifyUser):
+def san(string):
+    string = string.replace("'", "''")
+    return string
+
+def addArtist(artistID, userID):
     name = sp.artist(artistID)["name"].upper()
     types = ["album", "single", "compilation"]
     for albumType in types: #look at all album types
@@ -34,35 +39,49 @@ def addArtist(artistID, spotifyUser):
             albumName =  album["name"].upper()
             if "LIVE" not in albumName:
                 albumID = album["uri"]
-                addAlbum(albumID, dataBase)
+                try:
+                    addAlbum(albumID, userID)
+                except:
+                    pass
 
-
-def addAlbum(albumID, spotifyUser):
+def addAlbum(albumID, userID):
     name = sp.album(albumID)["name"].upper()
-    comments.append("FROM " + name + ":")
     songs = sp.album_tracks(albumID)
     for song in songs["items"]:
-        addSong(song["id"], dataBase)
+        try:
+            addSong(song["id"], userID)
+        except:
+            pass
 
 
-def addPlaylist(playlistID, spotifyUser):
+def addPlaylist(playlistID, userID):
     name = sp.playlist(playlistID)["name"].upper()
-    comments.append("FROM " + name + ":")
     songs = sp.playlist_tracks(playlistID)
     for song in songs["items"]:
         song = song["track"]
-        addSong(song["id"], dataBase)
+        try:
+            addSong(song["id"], userID)
+        except:
+            pass
+
+def songAdded(songID, userID):
+    songList = conn.execute("SELECT * FROM " + userID).fetchall()
+    songs = [item[0] for item in songList]
+    return songID in songs
 
 
-def addSong(songID, spotifyUser):
+def addSong(songID, userID):
+    if songAdded(songID, userID):
+        return
+    
     song = sp.track(songID)
     title = re.split(' - | \(', song["name"])[0].strip()
     
     #concat artists (and remove ',')
     artists = ""
     for artist in song["artists"]:
-        artists += artist["name"] + ","
-    artists = artists[:-1]
+        artists += artist["name"] + ", "
+    artists = artists[:-2]
 
     album = song["album"]["name"]
 
@@ -78,40 +97,110 @@ def addSong(songID, spotifyUser):
     else:
         cover = song['album']['images'][0]['url']
 
-    songString = "'"+songID+"','"+title+"','"+artists+"','"+album+"','"+cover+"','"+preview+"'"
-    print(songString)
-    insert_cmd = "INSERT INTO " + spotifyUser + f" VALUES ({songString})"
-
-    conn.execute(insert_cmd)
+    songString = "'"+san(songID)+"','"+san(title)+"','"+san(artists)+"','"+san(album)+"','"+san(cover)+"','"+san(preview)+"',0"
+    
+    conn.execute("INSERT INTO " + san(userID) + f" VALUES ({songString})")
     conn.commit()
 
 
+def removeSong(songID, userID):
+    if songAdded(songID, userID):
+        conn.execute("DELETE FROM "+san(userID)+" WHERE songID='"+san(songID)+"';")
+        conn.commit()    
+
+
+def removeAllSongs(userID):
+    songList = conn.execute("SELECT * FROM " + san(userID)).fetchall()
+    songs = [item[0] for item in songList]
+    for song in songs:
+        removeSong(song, userID)
+
+
+    
 '''
     This section focuses on adding/removing tables, and handling
     user info (usually background requests)
 '''
 
-def tableExists(spotifyUser):
+def tableExists(userID):
     tableList = conn.execute("SELECT * FROM sqlite_master WHERE type='table'").fetchall()
     tables = [item[1] for item in tableList]
-    return spotifyUser in tables
+    return userID in tables 
 
 
-def createTable(spotifyUser):
-    collumns = "songID TEXT UNIQUE, title TEXT, artist TEXT, album TEXT, cover TEXT, preview TEXT"
-    conn.execute("CREATE TABLE " + spotifyUser + " (" + collumns + ");")
+def createTable(userID):
+    collumns = "songID TEXT UNIQUE, title TEXT, artist TEXT, album TEXT, cover TEXT, preview TEXT, rank INTEGER"
+    conn.execute("CREATE TABLE " + san(userID) + " (" + collumns + ");")
 
+    conn.execute("INSERT INTO users VALUES ('"+san(userID)+"',0);")
     conn.commit()
 
 
-def getSongList(user):
-    return False
+def getSongList(userID):
+    songsBasic = conn.execute("SELECT * FROM " + san(userID)).fetchall()
+    songs = []
+    for song in songsBasic:
+        songs.append((song[0], song[1], song[2], song[3], song[4], song[5], song[6]))
+    return songs
+
+
+def songNumber(userID):
+    songsBasic = conn.execute("SELECT * FROM " + san(userID)).fetchall()
+    return len(songsBasic)
+
+
+#Update the current users last login day
+def updateUser(userID):
+    date = datetime.now()
+    conn.execute("UPDATE users SET active="+str(date.day)+" WHERE user='"+san(userID)+"';")
+    conn.commit()
     
 
+#This is a mark and sweep functionality 
+def sweepData():
+    date = datetime.now()
 
+    #create mark dictionary for all currently active user tables
+    tableList = conn.execute("SELECT * FROM sqlite_master WHERE type='table'").fetchall()
+    tables = {item[1]: False for item in tableList}
+    tables.pop("users")
+    
+    #mark live tables and remove dead users from user list
+    users = conn.execute("SELECT * FROM users").fetchall()
+    for user in users:
+        if (user[0] in tables) and abs(user[1] - date.day) <= 7:
+            tables[user[0]] = True
+        else:
+            conn.execute("DELETE FROM users WHERE user='" + san(user[0]) + "'")
+
+    #remove unmarked users from data table
+    for table in tables:
+        if tables[table] == False:
+            conn.execute("DROP TABLE " + san(table))
+
+    #finalize changes
+    conn.commit()
+
+#rerank song pOld to rank of pNew
+def updateSongRank(userID, pOld, pNew):
+    conn.execute("UPDATE "+userID+" SET rank="+str(pNew)+" WHERE rank="+str(pOld)+";")
+    conn.commit()
+
+
+#initialize ranking of songs randomly
+def initSongRank(userID):
+    songList = conn.execute("SELECT * FROM " + userID).fetchall()
+    songs = [item[0] for item in songList]
+    for i in range(len(songs)):
+        conn.execute("UPDATE "+userID+" SET rank="+str(i)+" WHERE songID='"+san(songs[i])+"';")
+    conn.commit()
 
     
+def getSongOfRank(userID, rank):
+    songsOfRank = conn.execute("SELECT * FROM " + userID + " WHERE rank=" + str(rank)).fetchall()
 
+    #get first song of correct rank
+    return songsOfRank[0]
     
     
 
--- PythonSongSorter/SpotifyRanking.py
+++ PythonSongSorter/SpotifyRanking.py
@@ -3,337 +3,153 @@ import random
 import string
 from flask import Flask, render_template, request, url_for, redirect, session
 import SongHandler as sh
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import RankingHandler as rh
 
 app = Flask(__name__)
 app.secret_key = 'rGRs001XJ1Y1M7jo'
 
 app.config["DEBUG"] = True
 
-comments = []
-songDict = dict()
-matches = dict()
-songs = []
-
-songFull = ["", ""]
-songNames = ["", ""]
-songCovers = ["", ""]
-songSamples = ["", ""]
-songArtists = ["", ""]
-
-songNum = 0
-maxSongNum = 0
-questionNum = 0
-
-finalList = []
-
 #WEBPAGE CODE
 #====================================================================================================================
 @app.route("/", methods=["GET", "POST"])
-def home():
-    global questionNum
-    
+def home():    
     #refresh page
     if request.method == "GET":
-        #initialize user id (not logged in! create random client_id)
+        #initialize user id and table (not logged in! create random client_id)
         if session.get("user") == None:
             session["user"] = "user" + ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(16))
-            if not sh.tableExists(session["user"]):
-                sh.createTable(session["user"])
+        if not sh.tableExists(session["user"]):
+            sh.createTable(session["user"])
 
+        if session.get("question") == None:
+            session["question"] = 0
+            
+        #refresh user entry data and sweep users
+        sh.updateUser(session["user"])
+        sh.sweepData()
+                
         
         #get message to display on website
         if session.get("message") == None:
             mess = ""
         else:
             mess = session["message"]
-            session["message"] = ""         
-
+            session["message"] = ""
+            
+        #get list of songs added by user
+        songList = sh.getSongList(session["user"])
+        
         #render HTML page for user
-        return render_template("main_page.html", comments=comments, message=mess, numsongs=str(len(songDict)))
+        return render_template("main_page.html", songs=songList, message=mess, cont=(session["question"]!=0))
     #button was pressed
     else:
         #Add songs buttons
-        if request.form["cmd"] == "Add Content":
-
-            #try logging user in
-            try:
-                sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(session["client_id"], session["client_secret"]))
-            except:
-                sp = spotipy.Spotify()
-
+        if request.form["cmd"] == "Add Content": 
             #add content to list
             try:
                 sh.addArtist(request.form["contents"], session["user"])
                 session["message"] = ""
+                session["question"] = 0
             except:
                 try:
                     sh.addAlbum(request.form["contents"], session["user"])
                     session["message"] = ""
+                    session["question"] = 0
                 except:
                     try:
                         sh.addPlaylist(request.form["contents"], session["user"])
                         session["message"] = ""
+                        session["question"] = 0
                     except:
                         try:
                             sh.addSong(request.form["contents"], session["user"])
                             session["message"] = ""
+                            session["question"] = 0
                         except:
                             session["message"] = "URL is not found! Double check the spelling, and make sure you have access to the URL."
 
-        #clear list
-        if request.form["cmd"] == "Reset List" or request.form["cmd"] == "Start Again":
-            refreshVars()
-            return redirect(url_for('home'))
+        #remove a single song from list
+        elif request.form["cmd"] == "remove":
+            session["message"] = "song removed!"
+            session["question"] = 0
+            sh.removeSong(request.form["contents"], session["user"])
 
 
+        #clear list of all songs
+        elif request.form["cmd"] == "Clear List":
+            session["message"] = "songs cleared!"
+            session["question"] = 0
+            sh.removeAllSongs(session["user"])
+
+
+        #Continue ranking songs
+        elif request.form["cmd"] == "Continue Ranking":
+            if session.get("question") == None or session["question"] == 0:
+                session["message"] = "No ranking session found!"
+            else:
+                return redirect(url_for('rank'))
+                
         #Begin ranking songs
-        if request.form["cmd"] == "Begin Ranking":
-            if len(songDict) > 0:
-                beginRanking()
+        elif request.form["cmd"] == "Begin Ranking":
+            if sh.songNumber(session["user"]) > 3:
+                session["start"], session["c0"], session["c1"], session["size"] = rh.startRanking(session["user"])
+                session["question"] = 1
                 return redirect(url_for('rank'))
             else:
-                session["message"] = "No songs selected!"
-
-#RANKING PAGES
-
-        if request.form["cmd"] == "Song 1":
-            if updateChoice("1"):
-                return redirect(url_for('rank'))
-            else:
-                finishRanking()
-                return redirect(url_for('final'))
-
-        if request.form["cmd"] == "Song 2":
-            if updateChoice("2"):
-                return redirect(url_for('rank'))
-            else:
-                finishRanking()
-                return redirect(url_for('final'))
-
-        if request.form["cmd"] == "Skip Question":
-            if updateChoice("skip"):
-                return redirect(url_for('rank'))
-            else:
-                finishRanking()
-                return redirect(url_for('final'))
+                session["message"] = "Please add at least 4 songs!"
 
         return redirect(url_for('home'))
 
 #--------------------------------------------------------
 
+#ranking page
 @app.route("/rank", methods=["GET", "POST"])
 def rank():
-    global maxSongNum, songNum
-    accuracy = min(95, round(maxSongNum/len(songDict)*100))
-    #ranking page
-    return render_template("rank_page.html", songNum=songNum, accuracy=accuracy, numsongs=str(len(songDict)), name1=songNames[0], name2=songNames[1], album1=songCovers[0], album2=songCovers[1], preview1=songSamples[0], preview2=songSamples[1], artist1=songArtists[0], artist2=songArtists[1], question=questionNum, )
+    if request.method == "GET":
+        #if ranking still needs to be done
+        if session["size"] <= sh.songNumber(session["user"]):
+            
+            #get next songs to compare
+            s0 = sh.getSongOfRank(session["user"], session["c0"])
+            s1 = sh.getSongOfRank(session["user"], session["c1"])
 
+            #determine accuracy
+            compsLeft = 0;
+            i = session["size"]
+            c0rel = session["c0"]-session["start"]
+            c1rel = session["c1"]-session["size"]-session["start"]
+            s = session["start"]+max(c0rel, c1rel)
+            while i < sh.songNumber(session["user"]):
+                compsLeft += (sh.songNumber(session["user"])-s)/i
+                i *= 2
+                s = 0
+            accuracy = round(100*(session["question"]-1)/(compsLeft + session["question"]-1))
+    
+            #go to ranking page
+            return render_template("rank_page.html", song0=s0, song1=s1, question=session["question"], accuracy = accuracy)
+        #ranking finished! go to ranking page!
+        else:
+            return redirect(url_for('final'))
+    else:
+        if request.form["cmd"] == "SONG 1":
+            reorder = False
+        else:
+            reorder = True
+        
+        session["question"] += 1
+        session["start"], session["c0"], session["c1"], session["size"] = rh.rankStep(session["start"], session["c0"], session["c1"], session["size"], sh.songNumber(session["user"]), reorder, session["user"])  
+        return redirect(url_for('rank'))
+
+
+def sortSongs(val):
+    return val[6]
+    
 @app.route("/final", methods=["GET", "POST"])
 def final():
+    finalList = sh.getSongList(session["user"])
+    finalList.sort(key=sortSongs)
     #final page
-    return render_template("final_page.html", final=finalList)
+    return render_template("final_page.html", songs=finalList)
 
 
-#SORTING PYTHON CODE
-#====================================================================================================================
-
-def refreshVars():
-    global songNum, maxSongNum, questionNum
-
-    comments.clear()
-    songDict.clear()
-
-    songNames.clear()
-    songNames.append("")
-    songNames.append("")
-
-    songCovers.clear()
-    songCovers.append("")
-    songCovers.append("")
-
-
-    songSamples.clear()
-    songSamples.append("")
-    songSamples.append("")
-
-    songArtists.clear()
-    songArtists.append("")
-    songArtists.append("")
-
-    finalList.clear()
-
-    songNum = 0
-    maxSongNum = 0
-    questionNum = 0
-
-def Probability(rating1, rating2):
-    return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating2 - rating1) / 25))
-
-
-
-
-def updateELO(song1, song2, songDict, answer, matches):
-    K = 25;
-    prob1win = Probability(songDict[song1][0], songDict[song2][0])
-    prob2win = Probability(songDict[song2][0], songDict[song1][0])
-    #update elo ratings
-
-    if answer == "1":
-        winningSong = song1
-        losingSong = song2
-    elif answer == "2":
-        winningSong = song2
-        losingSong = song1
-    else:
-        winningSong == "none"
-
-
-    if winningSong != "none":
-        matches[winningSong][0].add(losingSong)
-        matches[losingSong][1].add(winningSong)
-
-        loserLosesTo = set(matches[winningSong][1])
-        winnerWinsTo = set(matches[losingSong][0])
-
-        while len(winnerWinsTo) > 0:
-            addSong = list(winnerWinsTo).pop()
-            matches[winningSong][0].add(addSong)
-            winnerWinsTo.update(matches[addSong][0])
-            winnerWinsTo.remove(addSong)
-
-        while len(loserLosesTo) > 0:
-            addSong = list(loserLosesTo).pop()
-            matches[losingSong][1].add(addSong)
-            loserLosesTo.update(matches[addSong][1])
-            loserLosesTo.remove(addSong)
-
-
-    else:
-        matches[winningSong][2].add(losingSong)
-        matches[losingSong][2].add(winningSong)
-
-
-    if answer == "1":
-        songDict[song1][0] = songDict[song1][0] + K*(1-prob1win)
-        songDict[song2][0] = songDict[song2][0] + K*(0-prob2win)
-        if prob1win > 0.5:
-            return True
-    elif answer == "2":
-        songDict[song1][0] = songDict[song1][0] + K*(0-prob1win)
-        songDict[song2][0] = songDict[song2][0] + K*(1-prob2win)
-        if prob2win > 0.5:
-            return True
-    else:
-        return True
-
-    return False
-
-def beginRanking():
-    global songNum, questionNum, maxSongNum
-
-    questionNum = 0
-    maxSongNum = 0
-    songNum = 0
-    matches.clear()
-    for key in songDict:
-        matches[key] = (set(), set(), set())
-
-    songs.clear()
-    songs.extend([item[0] for item in sorted(songDict.items(), key=lambda x:x[1])])
-
-    getNewSongs()
-
-def updateList():
-    for key in matches:
-        wins = matches[key][0].copy()
-        losses = matches[key][1].copy()
-        for win in wins:
-            updateELO(key, win, songDict, "1", matches)
-        for loss in losses:
-            updateELO(key, loss, songDict, "2", matches)
-
-    songs.clear()
-    songs.extend([item[0] for item in sorted(songDict.items(), key=lambda x:x[1])])
-
-
-def getNewSongs():
-    global questionNum, maxSongNum
-
-    updateList()
-    songNum = 0
-
-    song1 = songs[songNum]
-    song2 = songs[songNum+1]
-
-    while (song2 in matches[song1][0]) or (song2 in matches[song1][1]) or (song2 in matches[song1][2]):
-        #update ELO
-        if song2 in matches[song1][0]: #song1 already wins
-            if Probability(songDict[song1][0], songDict[song2][0]) > 0.5:
-                songNum += 1
-            else:
-                songNum = 0
-                updateELO(song1, song2, songDict, "1", matches)
-
-        elif song2 in matches[song1][1]: #song2 already wins
-            if Probability(songDict[song2][0], songDict[song1][0]) > 0.5:
-                songNum += 1
-            else:
-                songNum = 0
-                updateELO(song1, song2, songDict, "2", matches)
-
-        if songNum >= len(songDict)-1:
-            return False
-        else:
-            songs.clear()
-            songs.extend([item[0] for item in sorted(songDict.items(), key=lambda x:x[1])])
-            song1 = songs[songNum]
-            song2 = songs[songNum+1]
-
-
-    if songNum > maxSongNum:
-        maxSongNum = songNum
-
-    questionNum += 1
-    songFull[0] = song1
-    songFull[1] = song2
-
-    songNames[0] = song1.split(":-:")[0]
-    songNames[1] = song2.split(":-:")[0]
-
-    songArtists[0] = song1.split(":-:")[1]
-    songArtists[1] = song2.split(":-:")[1]
-
-    songSamples[0] = songDict[song1][1]
-    songSamples[1] = songDict[song2][1]
-
-    songCovers[0] = songDict[song1][2]
-    songCovers[1] = songDict[song2][2]
-
-    return True
-
-
-def updateChoice(answer):
-    global songNum
-
-    if updateELO(songFull[0], songFull[1], songDict, answer, matches):
-        songNum += 1
-    else:
-        songNum = 0
-
-
-    return getNewSongs()
-
-
-def finishRanking():
-    finalList.clear()
-    sortedSongs = sorted(songDict.items(), key=lambda x:x[1])
-    minScore = sortedSongs[0][1][0]
-    maxScore = sortedSongs[len(sortedSongs)-1][1][0] - minScore
-
-    for i in range(len(sortedSongs)):
-        entry = sortedSongs[len(sortedSongs)-1-i]
-        songName = str(i+1) + ') ' + entry[0].split(":-:")[0]
-        entryScore = round((entry[1][0] - minScore)/maxScore*100)
-        finalList.append(songName + " - " + str(entryScore))
