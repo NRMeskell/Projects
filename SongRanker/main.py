0921fe882a82b3c7fa8d1702c6c6a2b607c2b81a
nmeskell <nathanmeskell@gmail.com>
2024-06-06 10:48:58 -0500

    save scroll position

--- SongRanker/main.py
+++ SongRanker/main.py
@@ -12,51 +12,50 @@ app.config["DEBUG"] = True
 
 #WEBPAGE CODE
 #====================================================================================================================
+def refreshHomePage():
+    #initialize user id and table (not logged in! create random client_id)
+    if session.get("user") == None:
+        session["user"] = "user" + ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(16))
+    if not sh.tableExists(session["user"]):
+        sh.createTable(session["user"])
+
+    #refresh user entry data and sweep users
+    sh.updateUser(session["user"])
+    sh.sweepData()
+
+
+    #get message to display on website
+    if session.get("message") == None:
+        mess = ""
+    else:
+        mess = session["message"]
+        session["message"] = ""
+
+
+    #initialize ranking variables
+    if session.get("question") == None:
+        session["question"] = 0
+    if session.get("start") == None:
+        session["start"] = 0
+    if session.get("c0") == None:
+        session["c0"] = 0
+    if session.get("c1") == None:
+        session["c1"] = 1
+    if session.get("size") == None:
+        session["size"] = 1
+
+    #get list of songs added by user
+    songList = sh.getSongList(session["user"])
+    songCount = sh.songNumber(session["user"])
+    compsNew = math.ceil(rh.approxQuestions(0, 0, 1, 1, sh.songNumber(session["user"])))
+    compsLeft = math.ceil(rh.approxQuestions(session["start"], session["c0"], session["c1"], session["size"], sh.songNumber(session["user"])))
+
+    return render_template("main_page.html", songs=songList, message=mess, cont=(session["question"]!=0), songNum=songCount, qNew=compsNew, qLeft=compsLeft)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    #refresh page
     if request.method == "GET":
-        #initialize user id and table (not logged in! create random client_id)
-        if session.get("user") == None:
-            session["user"] = "user" + ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(16))
-        if not sh.tableExists(session["user"]):
-            sh.createTable(session["user"])
-
-        #refresh user entry data and sweep users
-        sh.updateUser(session["user"])
-        sh.sweepData()
-
-
-        #get message to display on website
-        if session.get("message") == None:
-            mess = ""
-        else:
-            mess = session["message"]
-            session["message"] = ""
-
-
-        #initialize ranking variables
-        if session.get("question") == None:
-            session["question"] = 0
-        if session.get("start") == None:
-            session["start"] = 0
-        if session.get("c0") == None:
-            session["c0"] = 0
-        if session.get("c1") == None:
-            session["c1"] = 1
-        if session.get("size") == None:
-            session["size"] = 1
-
-        #get list of songs added by user
-        songList = sh.getSongList(session["user"])
-        songCount = sh.songNumber(session["user"])
-        compsNew = math.ceil(rh.approxQuestions(0, 0, 1, 1, sh.songNumber(session["user"])))
-        compsLeft = math.ceil(rh.approxQuestions(session["start"], session["c0"], session["c1"], session["size"], sh.songNumber(session["user"])))
-
-
-        #render HTML page for user
-        return render_template("main_page.html", songs=songList, message=mess, cont=(session["question"]!=0), songNum=songCount, qNew=compsNew, qLeft=compsLeft)
+        return refreshHomePage()
     #button was pressed
     else:
         #Add songs buttons
@@ -89,7 +88,7 @@ def home():
             session["message"] = "song removed!"
             session["question"] = 0
             sh.removeSong(request.form["contents"], session["user"])
-
+            redirect(url_for('home'))
 
         #clear list of all songs
         elif request.form["cmd"] == "Clear List":
@@ -114,9 +113,22 @@ def home():
             else:
                 session["message"] = "Please add at least 4 songs!"
 
-        return redirect(url_for('home'))
-
+        #refresh and render page
+        return refreshHomePage()
 #--------------------------------------------------------
+
+def refreshRankPage():
+    #get next songs to compare
+    s0 = sh.getSongOfRank(session["user"], session["c0"])
+    s1 = sh.getSongOfRank(session["user"], session["c1"])
+
+    #determine accuracy
+    compsLeft = rh.approxQuestions(session["start"], session["c0"], session["c1"], session["size"], sh.songNumber(session["user"]))
+    accuracy = round(100*(session["question"]-1)/(compsLeft + session["question"]-1))
+
+    #go to ranking page
+    return render_template("rank_page.html", song0=s0, song1=s1, question=session["question"], accuracy = accuracy)
+
 
 #ranking page
 @app.route("/rank", methods=["GET", "POST"])
@@ -124,18 +136,8 @@ def rank():
     if request.method == "GET":
         #if ranking still needs to be done
         if session["size"] < sh.songNumber(session["user"]):
-
-            #get next songs to compare
-            s0 = sh.getSongOfRank(session["user"], session["c0"])
-            s1 = sh.getSongOfRank(session["user"], session["c1"])
-
-            #determine accuracy
-            compsLeft = rh.approxQuestions(session["start"], session["c0"], session["c1"], session["size"], sh.songNumber(session["user"]))
-            accuracy = round(100*(session["question"]-1)/(compsLeft + session["question"]-1))
-
-            #go to ranking page
-            return render_template("rank_page.html", song0=s0, song1=s1, question=session["question"], accuracy = accuracy)
-        #ranking finished! go to ranking page!
+            return refreshRankPage()
+        #ranking finished! go to final page!
         else:
             return redirect(url_for('final'))
     else:
@@ -146,7 +148,7 @@ def rank():
 
         session["question"] += 1
         session["start"], session["c0"], session["c1"], session["size"] = rh.rankStep(session["start"], session["c0"], session["c1"], session["size"], sh.songNumber(session["user"]), reorder, session["user"])
-        return redirect(url_for('rank'))
+        return refreshRankPage()
 
 
 def sortSongs(val):
